e8743cc6799873a70fc74ce163830e1f56b9a175
deepakgiggs <deepakl@freshdesk.com>
2015-12-14 12:17:12 +0530

    interval 48hrs

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,37 +22,37 @@ class Post < ActiveRecord::Base
 =end
 
   def self.getAll()
-    Post.select('DISTINCT ON (url,created_at) *').where("created_at > (NOW() - INTERVAL '24 hour')").order('created_at DESC')
+    Post.select('DISTINCT ON (url,created_at) *').where("created_at > (NOW() - INTERVAL '48 hour')").order('created_at DESC')
   end
 
   def self.getByLocation(location)
     l = '%' + location + '%'
-    Post.select('DISTINCT ON (url,created_at) *').where("location ilike ? and created_at > (NOW() - INTERVAL '24 hour')", l).order('created_at DESC')
+    Post.select('DISTINCT ON (url,created_at) *').where("location ilike ? and created_at > (NOW() - INTERVAL '48 hour')", l).order('created_at DESC')
   end
 
   def self.getByLocations(locations)
     location = locations.join(' | ')
-    Post.select('DISTINCT ON (url,created_at) *').where("to_tsvector('english', location) @@ plainto_tsquery('english', ?) and created_at > (NOW() - INTERVAL '24 hour')",location).order('created_at DESC')
+    Post.select('DISTINCT ON (url,created_at) *').where("to_tsvector('english', location) @@ plainto_tsquery('english', ?) and created_at > (NOW() - INTERVAL '48 hour')",location).order('created_at DESC')
     #Post.where()
   end
 
   def self.getByMaterials(materials)
     puts materials
     material = materials.join(' | ')
-    Post.select('DISTINCT ON (url,created_at) *').where("to_tsvector('english', materials) @@ plainto_tsquery('english', ?) and created_at > (NOW() - INTERVAL '24 hour')",material).order('created_at DESC')
+    Post.select('DISTINCT ON (url,created_at) *').where("to_tsvector('english', materials) @@ plainto_tsquery('english', ?) and created_at > (NOW() - INTERVAL '48 hour')",material).order('created_at DESC')
   end
 
   def self.getByMaterial(material)
     material = '%' + material + '%'
     #puts material
-    Post.select('DISTINCT ON (url,created_at) *').where("materials ilike ? and created_at > (NOW() - INTERVAL '24 hour')", material).order('created_at DESC')
+    Post.select('DISTINCT ON (url,created_at) *').where("materials ilike ? and created_at > (NOW() - INTERVAL '48 hour')", material).order('created_at DESC')
   end
 
   def self.getByLocationsAndMaterials(locations, materials)
     location = locations
     material = materials.join(' | ')
     Post.select('DISTINCT ON (url,created_at) *').where("to_tsvector('english', location) @@ plainto_tsquery('english', ?) AND
-                to_tsvector('english', materials) @@ plainto_tsquery('english', ?) and created_at > (NOW() - INTERVAL '24 hour')",
+                to_tsvector('english', materials) @@ plainto_tsquery('english', ?) and created_at > (NOW() - INTERVAL '48 hour')",
                 location, material).order('created_at DESC')
   end
 end
